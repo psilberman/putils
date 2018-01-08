aa300bdf7ef0196705d344bc8282d23ca3c921b9
Peter Silberman <peter.silberman@expel.io>
2018-01-08 08:50:33 -0500

    added file with notes on plotly

---
+++ putils/plot.py
@@ -0,0 +1,12 @@
+'''
+Examples of using Plotly saving for later use
+
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.offline import download_plotlyjs, init_notebook_mode, plot, iplot
+
+init_notebook_mode(connected=True)
+
+plotly.offline.iplot(fig)
+'''
